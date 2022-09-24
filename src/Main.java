79c5df0bf7d3ed5b747df153d93ff172340d2d9d
bilal karahan <bilalkarahan309@gmail.com>
2022-09-24 06:05:15 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in=new Scanner(System.in);
+        System.out.println("Sayı giriniz : ");
+        int n= in.nextInt();
+        int x=0;
+        for(int i=1;n>i;i++){
+            if(n%i==0){
+               x+=i;
+            }
+        }
+        if(n==x){
+            System.out.println(n+" Mükemmel sayıdır.");
+        }else {
+            System.out.println(n+" Mükemmel sayı değildir. ");
+        }
     }
 }
